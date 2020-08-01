9603cd53c93b504015165a433ca05b345431e2f3
Patryk Zieliński <patryk.zielinski.stg@gmail.com>
2020-08-01 17:52:05 +0200

    Add main menu
    Added basic main menu without functionality and with simple design.

--- app/src/main/java/com/example/premia/MainActivity.java
+++ app/src/main/java/com/example/premia/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.premia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] menuItemsArr = getResources().getStringArray(R.array.main_menu_items);
+        List<String> menuItemsList = Arrays.asList(menuItemsArr);
+        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.menu_item, R.id.menuItemTextView, menuItemsList);
+
+        ListView menuList = findViewById(R.id.menuListView);
+        menuList.setAdapter(arrayAdapter);
     }
 }
