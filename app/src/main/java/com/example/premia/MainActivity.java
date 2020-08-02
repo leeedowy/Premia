2f207757a1f5cfde6db496bbbf1ec780afeef108
Patryk Zieliński <patryk.zielinski.stg@gmail.com>
2020-08-02 12:34:15 +0200

    Implement item listeners for main menu
    Also done some variable and file name refactoring.

--- app/src/main/java/com/example/premia/MainActivity.java
+++ app/src/main/java/com/example/premia/MainActivity.java
@@ -4,28 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "MainActivity";
 
-    private ArrayAdapter<String> arrayAdapter;
+    private ArrayAdapter<String> mainMenuAdapter;
+    private ListView mainMenu;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] menuItemsArr = getResources().getStringArray(R.array.main_menu_items);
-        List<String> menuItemsList = Arrays.asList(menuItemsArr);
-        arrayAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.menu_item, R.id.menuItemTextView, menuItemsList);
+        List<String> menuItemsList = Arrays.asList(getResources().getStringArray(R.array.main_menu_items));
+        mainMenuAdapter = new ArrayAdapter<>(getApplicationContext(), R.layout.main_menu_item, R.id.mainMenuItemTextView, menuItemsList);
 
-        ListView menuList = findViewById(R.id.menuListView);
-        menuList.setAdapter(arrayAdapter);
+        mainMenu = findViewById(R.id.menuListView);
+        mainMenu.setAdapter(mainMenuAdapter);
+        mainMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (position) {
+                    case 0:
+                        // TODO IMPLEMENT BONUS TRACKING
+                        break;
+                    case 1:
+                        // TODO IMPLEMENT HISTORY
+                        break;
+                    case 2:
+                        // TODO IMPLEMENT STATS
+                        break;
+                    default:
+                        Log.e(TAG, "Position " + position + " not supported.");
+                }
+            }
+        });
     }
 }
